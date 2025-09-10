934bd01826d3f5387bc664dd7c7ae6046a365282
JoeyScottSchronce <joeyscottschronce@gmail.com>
2025-09-10 10:35:25 -0400

    added config

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+from kivy.config import Config
+Config.set('graphics', 'fullscreen', 'auto')
+Config.set('graphics', 'resizable', True)
+
 from tic_tic_tic_tac_toe.ui import TicTacToeApp
+
 
 if __name__ == '__main__':
     TicTacToeApp().run()
